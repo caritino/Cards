ba719f7c01129936b137c7c8b7478d534656d874
Alejandro Rabadán Camacho <arabadan70@hotmail.com>
2016-06-27 17:24:14 -0500

    Adding stuff to main

--- testCards.java
+++ testCards.java
@@ -32,9 +32,43 @@ public class testCards {
         }
     }
     
+    public static void leeCartas(ArrayList<Cards> mano, Cards cartaCompara, Deck baraja){
+        if( comparaCartas(mano.get(0), cartaCompara) == 1){
+            cartaCompara = mano.get(0);
+            mano.remove(0);
+        } else if( comparaCartas(mano.get(1), cartaCompara) == 1){
+            cartaCompara = mano.get(1);
+            mano.remove(1);
+        } else if( comparaCartas(mano.get(2), cartaCompara) == 1){
+            cartaCompara = mano.get(2);
+            mano.remove(2);
+        } else if(mano.size() == 3){
+            mano.add(baraja.drawFromDeck());
+            
+            if( comparaCartas(mano.get(3), cartaCompara) == 1){
+                cartaCompara = mano.get(3);
+                mano.remove(3);
+            }
+        } else if(mano.size() == 4){
+            mano.add(baraja.drawFromDeck());
+            
+            if( comparaCartas(mano.get(4), cartaCompara) == 1){
+                cartaCompara = mano.get(4);
+                mano.remove(4);
+            }
+        } else if(mano.size() == 5){
+            mano.add(baraja.drawFromDeck());
+            
+            if( comparaCartas(mano.get(5), cartaCompara) == 1){
+                cartaCompara = mano.get(5);
+                mano.remove(5);
+            }
+        }
+        System.out.println("Mano al terminar turno: "+mano.toString());
+        System.out.println("Nueva carta referencia: "+cartaCompara);
+    }
+    
     public static void main(String[] args) {
-        
-        int i;
         
         // Se declara el deck
         Deck mazo = new Deck();
@@ -44,31 +78,19 @@ public class testCards {
         ArrayList<Cards> manoUser = new ArrayList<Cards>();
         ArrayList<Cards> manoIA = new ArrayList<Cards>();
         
+        
         // Se sacan las dos manos del mazo
         drawCards(mazo, manoUser);
         drawCards(mazo, manoIA);
         
         cartaInicial = mazo.drawFromDeck();
         
-        System.out.println(cartaInicial.toString());
-        System.out.println(manoIA.toString());
+        System.out.println("Carta inicial: "+cartaInicial.toString());
+        System.out.println("Mano inicial de la IA:"+manoIA.toString());
         
-        for(i = 0 ; i < manoIA.size(); i++ ){
-            if( comparaCartas(manoIA.get(i), cartaInicial) == 1){
-                manoIA.remove(i);
-            }
-            System.out.println(manoIA.toString());
-            break;
-            
-        }
+        leeCartas(manoIA, cartaInicial, mazo);
         
-        System.out.println("ManoIA fuera del for: "+manoIA.toString());
-        
-        
-//        System.out.println(manoIA.toString());
-//        System.out.println(manoIA.get(0).toString());
-//        manoIA.remove(0);
-//        System.out.println(manoIA.toString());
 
-    }    
+
+    }
 }
