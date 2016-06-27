cc2682ea1288ae9dfbd6cd1866f1f412530a4c56
Alejandro Rabadán Camacho <arabadan70@hotmail.com>
2016-06-27 16:15:01 -0500

    Se empieza a implemetar el core

--- testCards.java
+++ testCards.java
@@ -5,45 +5,70 @@
  */
 package cards;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  *
  * @author araba
  */
 public class testCards {
+    
+    public static int comparaCartas(Cards carta1, Cards carta2){
+        if(carta1.getRank() == carta2.getRank() || carta1.getSuit() == carta2.getSuit()){
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    public static void drawCards(Deck baraja, ArrayList<Cards> mano){
+        int i = 0;
+        while(i < 3)
+        {
+            mano.add(baraja.drawFromDeck());
+            i++;
+        }
+    }
+    
     public static void main(String[] args) {
         
-//        Cards baraja = new Cards(1,0);
-//        System.out.println(baraja.toString());
-    
-    
-//            
-//		
-//
-//		System.out.println( deck.getTotalCards() );
-//
-//	   while (deck.getTotalCards()!= 0 )
-//	   {
-//		   C = deck.drawFromDeck();
-//		   System.out.println( C.toString() );
-//           }    
-//    }
+        int i;
+        
+        // Se declara el deck
+        Deck mazo = new Deck();
+        Cards cartaInicial;
 
-        Hand mano = new Hand();
-        Deck deck = new Deck();
-        Cards baraja;
+        // Se declaran las dos manos
+        ArrayList<Cards> manoUser = new ArrayList<Cards>();
+        ArrayList<Cards> manoIA = new ArrayList<Cards>();
         
-        System.out.println(Arrays.toString(mano.getHand()));
-        System.out.println(Arrays.toString(mano.getHand()));
-        System.out.println("");
+        // Se sacan las dos manos del mazo
+        drawCards(mazo, manoUser);
+        drawCards(mazo, manoIA);
+        
+        cartaInicial = mazo.drawFromDeck();
+        
+        System.out.println(cartaInicial.toString());
+        System.out.println(manoIA.toString());
+        
+        for(i = 0 ; i < manoIA.size(); i++ ){
+            if( comparaCartas(manoIA.get(i), cartaInicial) == 1){
+                manoIA.remove(i);
+            }
+            System.out.println(manoIA.toString());
+            break;
+            
+        }
+        
+        System.out.println("ManoIA fuera del for: "+manoIA.toString());
         
         
-        while (deck.getTotalCards()!= 0 )
-	   {
-		   baraja = deck.drawFromDeck();
-		   System.out.println( baraja.toString() );
-           }    
+//        System.out.println(manoIA.toString());
+//        System.out.println(manoIA.get(0).toString());
+//        manoIA.remove(0);
+//        System.out.println(manoIA.toString());
 
     }    
 }
